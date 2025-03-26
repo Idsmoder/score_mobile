f0ccaa5ced10dee53c53e616ec76f61c96274f51
Bexzod <141816059+bexzodbek9866@users.noreply.github.com>
2025-03-26 21:02:53 +0500

    web viewga qo'shildi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,122 +3,146 @@ import api from "@/config/api";
 import {useEffect, useState} from "react";
 import {router} from "expo-router";
 import ModalComponent from "@/components/ModalComponent";
+import { LinearGradient } from "expo-linear-gradient";
 const { height } = Dimensions.get('window');
- const HomeScreen = () => {
-    const [list, setList] = useState([]);
-     const [modalVisible, setModalVisible] = useState(false);
-     const [selectedItem, setSelectedItem] = useState(null);
-     const [slideAnim] = useState(new Animated.Value(height));
+import { format } from "date-fns";
+const HomeScreen = () => {
+  const [list, setList] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [slideAnim] = useState(new Animated.Value(height));
 
-     const showModal = (item) => {
-         setSelectedItem(item);
-         setModalVisible(true);
-         Animated.timing(slideAnim, {
-             toValue: 0,
-             duration: 300,
-             useNativeDriver: true,
-         }).start();
-     };
+  const showModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+    Animated.timing(slideAnim, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
-     const hideModal = () => {
-         Animated.timing(slideAnim, {
-             toValue: height,
-             duration: 300,
-             useNativeDriver: true,
-         }).start(() => {
-             setModalVisible(false);
-             setSelectedItem(null);
-         });
-     };
-     const getList = async () => {
-         const response = await api.get('app/history/list');
-            setList(response.data.data);
-     }
-     useEffect(() => {
-            getList();
-     }, []);
-     const renderItem = ({ item }) => (
-         <TouchableOpacity onPress={() => showModal(item)}>
-         <View style={styles.item}>
-             <Text style={styles.title}>ID Пациента: {item.id}</Text>
-             <Text>Время приёма: {new Date(item.created_at).toLocaleString()}</Text>
-         </View>
- </TouchableOpacity>
-     );
+  const hideModal = () => {
+    Animated.timing(slideAnim, {
+      toValue: height,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      setModalVisible(false);
+      setSelectedItem(null);
+    });
+  };
+  const getList = async () => {
+    const response = await api.get('app/history/list');
+    setList(response.data.data);
+  }
+  useEffect(() => {
+    getList();
+  }, []);
+  const formatDate = (created_at) => {
+    return format(new Date(created_at), "dd.MM.yyyy HH:mm");
+  };
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => showModal(item)}>
+      <LinearGradient
+        colors={["#4facfe", "#8c4fbf"]}
+        style={styles.gradient}
+      >
+        <View style={styles.item}>
+          <Text style={styles.title}>ID Пациента: {item.id}</Text>
+          <Text>Время приёма: {formatDate(item.created_at)}</Text>
+        </View>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
 
   return (
-      <SafeAreaView style={styles.container}>
-
-          <Text style={styles.header}>Информация о приёме пациента</Text>
-          <FlatList
-              data={list}
-              renderItem={renderItem}
-              keyExtractor={item => item.id.toString()}
-          />
-          <Modal animationType="none" transparent={true} visible={modalVisible} onRequestClose={hideModal}>
-              <ModalComponent modalVisible={modalVisible} hideModal={hideModal} slideAnim={slideAnim} item={selectedItem} />
-          </Modal>
-
- </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Информация о приёме пациента</Text>
+      <FlatList
+        data={list}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+      <Modal 
+        animationType="none" 
+        transparent={true} 
+        visible={modalVisible} 
+        onRequestClose={hideModal}
+      >
+        <ModalComponent 
+          modalVisible={modalVisible} 
+          hideModal={hideModal} 
+          slideAnim={slideAnim} 
+          item={selectedItem} 
+        />
+      </Modal>
+    </SafeAreaView>
   );
 }
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#E0F7FA',
+  container: {
+    flex: 1,
+    backgroundColor: '#E0F7FA',
+    paddingTop: 20,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#2196F3',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    width: '100%',
+    height: '90%',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    button: {
-        padding: 10,
-        backgroundColor: '#2196F3',
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: '#fff',
-    },
-    centeredView: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    modalView: {
-        width: '100%',
-        height: '90%',
-        backgroundColor: 'white',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: 'center',
-    },
-    header: {
-        fontSize: 28,
-        textAlign: 'center',
-        marginVertical: 20,
-        color: '#01579B',
-    },
-    item: {
-        backgroundColor: '#81D4FA',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        borderRadius: 10,
-    },
-    title: {
-        fontSize: 24,
-        color: '#0277BD',
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 10,
+    paddingTop: 20,
+    color: '#01579B',
+  },
+  item: {
+    marginVertical: 8,
+    marginHorizontal: 5,
+    borderRadius: 10,
+    backgroundColor: "transparent",
+  },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+  },
+  gradient: {
+    margin: 10,
+    borderRadius: 12,
+    padding: 2,
+  },
 });
--- components/ModalComponent.jsx
+++ components/ModalComponent.jsx
@@ -1,78 +1,90 @@
 import {Modal, Text, Animated, View, TouchableOpacity,StyleSheet} from "react-native";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import api from "@/config/api";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
-
+import { WebView } from "react-native-webview";
+import * as Secure from 'expo-secure-store';
 const ModalComponent = ({modalVisible,hideModal,slideAnim,item})=> {
-
-    useEffect(() => {
-        const getItemDoc = (item) => {
-            const res = api.get(`app/history/show/${item?.doc_id}`);
-            console.log(res, 'sass');
-        }
-        getItemDoc(item);
-    }, []);
-    return (
-        <View style={styles.centeredView}>
-            <Animated.View style={[styles.modalView, { transform: [{ translateY: slideAnim }] }]}>
-                <Text style={styles.modalText}>Привет, это модальное окно снизу вверх!</Text>
-                <TouchableOpacity style={styles.closeButton} onPress={hideModal}>
-                    <Text style={styles.buttonText}>Закрыть</Text>
-                </TouchableOpacity>
-
-            </Animated.View>
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    // const getItemDoc = (item) => {
+    //   const res = api.get(`app/history/show/${item?.doc_id}`);
+    // }
+    // getItemDoc(item);
+    getToken = async () => {
+      const Token = await Secure.getItemAsync('accessToken');
+      setToken(Token);
+    }
+    getToken();
+  }, []);
+  return (
+    <View style={styles.centeredView}>
+      <Animated.View style={[styles.modalView, { transform: [{ translateY: slideAnim }] }]}>
+        <Text style={styles.modalText}>Привет, это модальное окно снизу вверх!</Text>
+        <View style={styles.webview}>
+          <WebView source={{ uri: `https://score.gx.uz/webView/${item.id}?token=${token}` }} />
         </View>
-    )
+        <TouchableOpacity style={styles.closeButton} onPress={hideModal}>
+          <Text style={styles.buttonText}>Закрыть</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </View>
+  )
 }
 const styles = StyleSheet.create({
-    centeredView: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    width: '100%',
+    height: '95%',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    modalView: {
-        width: '100%',
-        height: '95%',
-        backgroundColor: 'white',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-    closeButton: {
-        position: 'absolute',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        bottom: 20,
-        padding: 10,
-        backgroundColor: '#f44336',
-        borderRadius: 5,
-    },
-    closeButtonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: 'center',
-    },
-    button: {
-        padding: 10,
-        backgroundColor: '#2196F3',
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: '#fff',
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  closeButton: {
+    position: 'absolute',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 20,
+    padding: 10,
+    backgroundColor: '#f44336',
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  webview: {
+    width: '100%',
+    height: '90%',
+    marginBottom: 20,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#2196F3',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+  },
 });
 export default ModalComponent;
